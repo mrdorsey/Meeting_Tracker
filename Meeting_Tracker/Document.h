94e8346cd5103445844cc447e1e877a08eeb5d8d
michael.r.dorsey <michael.r.dorsey@gmail.com>
2012-10-08 20:45:20 -0700

    Initial Commit

---
+++ Meeting_Tracker/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Meeting_Tracker
+//
+//  Created by Michael Dorsey on 10/8/12.
+//  Copyright (c) 2012 Michael Dorsey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Meeting_TrackerTests/Meeting_TrackerTests.m
@@ -0,0 +1,32 @@
+//
+//  Meeting_TrackerTests.m
+//  Meeting_TrackerTests
+//
+//  Created by Michael Dorsey on 10/8/12.
+//  Copyright (c) 2012 Michael Dorsey. All rights reserved.
+//
+
+#import "Meeting_TrackerTests.h"
+
+@implementation Meeting_TrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Meeting_TrackerTests");
+}
+
+@end
